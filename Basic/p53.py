d39279753ff3ee8df1ee4878afa204e012a1520d
Abdhesh <abdheshshah111@gmail.com>
2025-11-25 15:35:29 +0545

    done

--- Basic/p53.py
+++ Basic/p53.py
@@ -1,1 +1,14 @@
-# Auto-generated file
+class car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year= year
+    
+    def car_info(self):
+        print(f"The brand of car is {self.brand} \nModel is {self.model} \nThe year of lunch is {self.year}\n")
+
+p1 = car("Mercedes-Benz"," 300 SL Gullwing", 1955)
+p2 = car("Ford","Model A", 1903)
+
+p1.car_info()
+p2.car_info()
--- Basic/p54.py
+++ Basic/p54.py
@@ -1,1 +1,21 @@
-# Auto-generated file
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def great(self):
+        print(f"Hi, I am {self.name}")
+
+
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self.grade = grade
+
+    def Study(self):
+        print(f"I am studying grade {self.grade}")
+
+
+a=Student("Abdhesh",22,"Bechlor")    
+a.great() 
+a.Study()           
