01e9f5c2c77c0eff79dd24edeeff57b1e9731128
Abdhesh <abdheshshah111@gmail.com>
2025-12-17 14:27:06 +0545

    upgrading with function

--- Advance/Project_1.py
+++ Advance/Project_1.py
@@ -1,50 +1,50 @@
 # Simple Calculator
-xyz = True
-while xyz:
-    abc = True
-    while abc:
-        print("Select operation:")
-        print("1. Add")
-        print("2. Subtract")
-        print("3. Multiply")
-        print("4. Divide")
+import math
+def add(x, y): return x+y
+def subtract(x, y): return x-y
+def product(x, y): return x*y
+def divide(x, y): return "Error: Division by zero is not allowed" if y == 0 else x/y
+def power(x, y): return x**y
+def modulus(x, y): return "Error: Division by zero is not allowed" if y == 0 else x % y
+def square_root(x): return "Error: Negative Number" if x < 0 else math.sqrt(x)
 
-        choice = input("Enter choice (1/2/3/4): ")
-        try:
+
+def calcultion():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power(x^y)")
+    print("6. Modulus(x%y)")
+    print("7. Square Root(x**y)")
+
+    choice = input("Enter choice (1-7): ")
+    try:
+        if choice=='7':
+            num1 = float(input("\nEnter number: "))
+            print(f"Result: √{num1} = {square_root(num1)}")
+        else:
             num1 = float(input("\nEnter first number: "))
             num2 = float(input("Enter second number: "))
-        except:
-            print("Enter valid input")
-            continue
-        
+    
+
         if choice == '1':
-            abc=False
-            print(f"Result: {num1} + {num2} = {num1 + num2}") 
+            print(f"Result: {num1} + {num2} = {add(num1, num2)}")
         elif choice == '2':
-            abc=False
-            print(f"Result: {num1} - {num2} = {num1 - num2}")
+            print(f"Result: {num1} - {num2} = {subtract(num1, num2)}")
         elif choice == '3':
-            abc=False
-            print(f"Result: {num1} * {num2} = {num1 * num2}")
+            print(f"Result: {num1} * {num2} = {product(num1, num2)}")
         elif choice == '4':
-            abc=False
-            if num2 != 0:
-                print(f"Result: {num1} / {num2} = {num1 / num2}")
-            else:
-                print("Error: Division by zero is not allowed.")
+            print(f"Result: {num1} / {num2} = {divide(num1, num2)}")
+        elif choice == '5':
+            print(f"Result: {num1} ** {num2} = {power(num1, num2)}")
+        elif choice == '6':
+            print(f"Result: {num1} % {num2} = {modulus(num1, num2)}")
+        elif choice == '7':
+            print(f"Result: {num1} = {square_root(num1)}")
+
         else:
             print("Invalid input\n")
-            continue
-
-    do = True
-    while do:
-        mood = input("\nWant to continue (Yes/No): ").lower()
-        if mood == "yes":
-            print("Okay, continuing...")
-            do = False
-        elif mood == "no":
-            print("Exiting program...")
-            xyz = False
-            do = False
-        else:
-            print("Invalid input, please type Yes or No.")
+    except:
+        print("Enter valid input")
