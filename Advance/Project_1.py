70f0ddc456c5f13e232145caee078319afd311b2
Abdhesh <abdheshshah111@gmail.com>
2025-12-17 14:36:35 +0545

    upgrading

--- Advance/Project_1.py
+++ Advance/Project_1.py
@@ -5,11 +5,11 @@ def subtract(x, y): return x-y
 def product(x, y): return x*y
 def divide(x, y): return "Error: Division by zero is not allowed" if y == 0 else x/y
 def power(x, y): return x**y
-def modulus(x, y): return "Error: Division by zero is not allowed" if y == 0 else x % y
+def modulus(x,y): return "Error: Division by zero is not allowed" if y == 0 else x % y
 def square_root(x): return "Error: Negative Number" if x < 0 else math.sqrt(x)
 
 
-def calcultion():
+def calculation():
     print("Select operation:")
     print("1. Add")
     print("2. Subtract")
@@ -21,13 +21,12 @@ def calcultion():
 
     choice = input("Enter choice (1-7): ")
     try:
-        if choice=='7':
+        if choice == '7':
             num1 = float(input("\nEnter number: "))
             print(f"Result: √{num1} = {square_root(num1)}")
         else:
             num1 = float(input("\nEnter first number: "))
             num2 = float(input("Enter second number: "))
-    
 
         if choice == '1':
             print(f"Result: {num1} + {num2} = {add(num1, num2)}")
@@ -45,3 +44,19 @@ def calcultion():
             print("Invalid input\n")
     except:
         print("Enter valid input")
+
+
+xyz = True
+while xyz:
+    calculation()
+    while True:
+        mood = input("\nWant to continue (Yes/No): ").lower()
+        if mood == "yes":
+            print("Okay, continuing...")
+            break
+        elif mood == "no":
+            print("Exiting program...")
+            xyz = False
+            break
+        else:
+            print("Invalid input, please type Yes or No.")
